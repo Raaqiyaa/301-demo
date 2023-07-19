13936ad0735f1f220cadacd17b9f35c2fa697b82
raaqiya ali <raaqiyaali_@hotmail.co.uk>
2023-07-19 13:28:57 +0100

    update

---
+++ src/component/footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+    return (
+        <footer>
+        <span>&#169; Horned Beast</span>
+        </footer>
+    );
+}
