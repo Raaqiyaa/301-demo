a0c0b936035c9fba03a9962cdb9cb35a365b8a12
raaqiya ali <raaqiyaali_@hotmail.co.uk>
2023-07-21 16:24:02 +0100

    update

--- src/App.js
+++ src/App.js
@@ -3,12 +3,37 @@ import "./App.css";
 import Header from "./component/header";
 import Footer from "./component/footer";
 import Main from "./component/main";
+import SelectedBeast from "./compotent/SelectedBeast";
+
+import data from "./data.json";
+import { useState } from "react";
 
 function App() {
+  const [modal, setModal] = useState(false);
+  const [modalContent, setModalContent] = useState({});
+  const [beastData, setBeastData] = useState(data);
+
+  function handleModal(beast) {
+    setModal(!modal);
+    setModalContent(beast);
+  }
+
+  function closeModal() {
+    setModal(!modal);
+    setModalContent({});
+  }
+
+  function handleBeasts(event) {
+    let beastNum = parseInt(event.target.value);
+    const filteredBeasts = data.filter((beast) => beast.horns === beastNum);
+    event.target.value === "" ? setBeastData(data) : setBeastData(filteredBeasts);
+  }
+
   return (
     <div className="App">
-      <Header />
-      <Main />
+      <Header  handleBeasts={handleBeasts}/>
+      <Main beastData={beastData} handleModal={handleModal}/>
+      {modal && <SelectedBeast modalContent={modalContent} closeModal={closeModal} />}
       <Footer />
     </div>
   );
--- src/component/selected.js
+++ src/component/selected.js
@@ -2,16 +2,16 @@ import React from "react";
 import "./SelectedBeast.css";
 
 export default function SelectedBeast({ modalContent, closeModal }) {
-  return (
+    return (
     <div className="selected-beast">
-      <div className="sb-container">
+        <div className="sb-container">
         <img src={modalContent.image_url} alt={modalContent.title} />
         <h3>{modalContent.title}</h3>
         <p>{modalContent.description}</p>
         <span className="cross" onClick={closeModal}>
-          X
+        X
         </span>
-      </div>
+        </div>
     </div>
-  );
+    );
 }
